e1b708cd761118ad55f475f696e0ac5c3ddb9489
James Davis <james.davis1@citrix.com>
2018-07-17 16:34:44 +0100

    Reqorked for OOP and changed setting imput etc
    Signed-off-by: James Davis <james.davis1@citrix.com>

--- tracker.py
+++ tracker.py
@@ -1,124 +1,88 @@
-# python tracker.py --filter HSV --webcam
-
 import cv2
 import argparse
 import numpy as np
+import imutils
+import json
+import time
 
 
-def callback(value):
-    pass
+class Game:
+
+    def get_arguments(self):
+        ap = argparse.ArgumentParser()
+        ap.add_argument('-l', '--live', required=False,
+                        help='Use live video source', action='store_true')
+        ap.add_argument('-i', '--image', required=False,
+                        help='Still image - for debugging')
+        ap.add_argument('-v', '--video', required=False,
+                        help='Use pre-recorded video source')
+        args = vars(ap.parse_args())
+
+        # TODO - Add checks for file extensions
+        return args
+
+    def loadSettings(self):
+        with open('settings.json') as f:
+            data = json.load(f)
+        return data['rgb'], data['hsv'], data['values']
+
+    def __init__(self):
+        self.rgb, self.hsv, self.filterValues = self.loadSettings()
+        self.args = self.get_arguments()
+
+    def image(self):
+        # Show ball tracking for single image
+        img = cv2.imread(self.args['image'], 0)
+        imgS = cv2.resize(img, (960, 540))
+        cv2.imshow('image', imgS)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def video(self):
+        # Ball tracking using a pre-recorded video source
+        if self.hsv:
+            ballLower = self.filterValues[:3]
+            ballHigher = self.filterValues[3:]
+
+            stream = cv2.VideoCapture(self.args['video'])
+            time.sleep(2.0)
+
+            while True:
+                frame = stream.read()
+                if frame is None:
+                    break
+
+                frame = imutils.resize(frame, width=600)
+                blur = cv2.GaussianBlur(frame, (11, 11), 0)
+                hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+
+                mask = cv2.inRange(hsv, ballLower, ballHigher)
+                mask = cv2.erode(mask, None, iterations=2)
+                mask = cv2.dilate(mask, None, iterations=2)
 
 
-def setup_trackbars(range_filter):
-    cv2.namedWindow("Trackbars", 0)
 
-    for i in ["MIN", "MAX"]:
-        v = 0 if i == "MIN" else 255
+        else:
+            raise NotImplementedError('RGB filters are not implemented yet. Please use HSV')
 
-        for j in range_filter:
-            cv2.createTrackbar("%s_%s" % (j, i), "Trackbars", v, 255, callback)
+    def live(self):
+        # Ball tracking using a live video source
+        if self.hsv:
+            pass
+        else:
+            raise NotImplementedError('RGB filters are not implemented yet. Please use HSV')
 
-
-def get_arguments():
-    ap = argparse.ArgumentParser()
-    ap.add_argument('-f', '--filter', required=False,
-                    help='Range filter. RGB or HSV')
-    ap.add_argument('-w', '--webcam', required=False,
-                    help='Use webcam', action='store_true')
-    ap.add_argument('-i', '--image', required=False,
-                    help='Still image - for debugging')
-    args = vars(ap.parse_args())
-
-    #if not args['filter'].upper() in ['RGB', 'HSV']:
-        #ap.error("Please speciy a correct filter.")
-
-    return args
-
-
-def get_trackbar_values(range_filter):
-    values = []
-
-    for i in ["MIN", "MAX"]:
-        for j in range_filter:
-            v = cv2.getTrackbarPos("%s_%s" % (j, i), "Trackbars")
-            values.append(v)
-    return values
-
-
-def oldMain():
-    args = get_arguments()
-
-    range_filter = args['filter'].upper()
-
-    camera = cv2.VideoCapture(0)
-
-    setup_trackbars(range_filter)
-
-    while True:
-        if args['webcam']:
-            ret, image = camera.read()
-
-            if not ret:
-                break
-
-            if range_filter == 'RGB':
-                frame_to_thresh = image.copy()
-            else:
-                frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-        v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(range_filter)
-
-        thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
-
-        kernel = np.ones((5, 5), np.uint8)
-        mask = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-
-        # find contours in the mask and initialize the current
-        # (x, y) center of the ball
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        center = None
-
-        # only proceed if at least one contour was found
-        if len(cnts) > 0:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
-            c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-            # only proceed if the radius meets a minimum size
-            if radius > 10:
-                # draw the circle and centroid on the frame,
-                # then update the list of tracked points
-                cv2.circle(image, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                cv2.circle(image, center, 3, (0, 0, 255), -1)
-                cv2.putText(image, "centroid", (center[0] + 10, center[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255),
-                            1)
-                cv2.putText(image, "(" + str(center[0]) + "," + str(center[1]) + ")", (center[0] + 10, center[1] + 15),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 1)
-
-        # show the frame to our screen
-        cv2.imshow("Original", image)
-        cv2.imshow("Thresh", thresh)
-        cv2.imshow("Mask", mask)
-
-        if cv2.waitKey(1) & 0xFF is ord('q'):
-            break
-
-
-def main():
-    args = get_arguments()
-
-    img = cv2.imread(args['image'],0)
-
-    imgS = cv2.resize(img, (960, 540))
-    cv2.imshow('image', imgS)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    def run(self):
+        if self.args.get('image', False):
+            self.image()
+        elif self.args.get('video', False):
+            self.video()
+        elif self.args.get('live', False):
+            self.live()
+        else:
+            raise ValueError('Either Image, Video or Webcam not specified. At-least one needed')
 
 
 if __name__ == '__main__':
-    main()
+    game = Game()
+    game.run()
