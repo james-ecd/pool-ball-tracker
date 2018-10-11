75876a5aaef0f62c570b92f6b01fe7f285ad56bd
James Davis <james.davis1@citrix.com>
2018-10-11 16:53:30 +0100

    Added demo test file and requirements
    Signed-off-by: James Davis <james.davis1@citrix.com>

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from tracker import Game
 
 
+authToken = 'xoxb-2527431116-453636589221-mmnC9RZTsEpywUoHhoVaSVpi'
+
 def testRun():
     game = Game()
     print(game.videoCount('resources\\video-1531832402.mp4'))
